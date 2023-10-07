fee547cecdfedc8bc8a795a5ceab26f80413b592
Shane <shane.pepperell@me.com>
2023-10-07 02:26:09 +0100

    more functions

--- Oblivilog.py
+++ Oblivilog.py
@@ -56,19 +56,50 @@ def save_user_list_to_file(message_count, filename):
     except Exception as e:
         print(f"Error writing to file {filename}: {str(e)}")
 
+def plot_messages_over_time(chat_df):
+    chat_df.set_index('timestamp', inplace=True)
+    messages_over_time = chat_df.resample('T').size() # 'T' for minute intervals
+    plt.figure(figsize=(12, 7))
+    plt.plot(messages_over_time.index, messages_over_time.values, color='skyblue')
+    plt.title('Messages Over Time')
+    plt.xlabel('Time')
+    plt.ylabel('Number of Messages')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
+def count_emote_usage(chat_df, emotes):
+    emote_counts = {emote: chat_df['message'].apply(lambda x: x.count(emote)).sum() for emote in emotes}
+    return pd.Series(emote_counts)
+
+def append_totals_to_file(message_count_per_user, filename):
+    with open(filename, 'a', encoding='utf-8') as file:
+        file.write(f"\nTotal Messages Sent: {message_count_per_user.sum()}\n")
+        file.write(f"Total Participants: {len(message_count_per_user)}\n")
+
 # Main Execution
 if __name__ == "__main__":
     file_paths = glob.glob('chattrans/*.txt')
     chat_df = concatenate_dfs(file_paths)
     
-    chat_df['timestamp'] = pd.to_datetime(chat_df['timestamp'], format="%H:%M:%S", errors='coerce')
-
-
+    # Ensure that your timestamp format matches the actual format in your data
+    chat_df['timestamp'] = pd.to_datetime(chat_df['timestamp'], errors='coerce')
 
     message_count_per_user = analyze_data(chat_df)
-    top_users = message_count_per_user.head(50)  # Adjust as per requirement
+    top_users = message_count_per_user.head(50)
     
     visualize_top_users(top_users)
+    plot_messages_over_time(chat_df)
     
-    # Save the full user list to a text file
+    # Emote Usage
+    emotes = ["oblivi118WINK", "oblivi118Lighter", "oblivi118Hands", "oblivi118Gun",
+              "oblivi118Cozy", "oblivi118Cookie", "oblivi118Lurking", "oblivi118Giggle",
+              "oblivi118Sip", "oblivi118Pat", "oblivi118Sing", "oblivi118Heart",
+              "oblivi118Blush", "oblivi118Huh", "oblivi118Lol", "oblivi118Hehe",
+              "oblivi118What", "oblivi118Evil", "oblivi118Zzz"]
+    emote_usage = count_emote_usage(chat_df, emotes)
+    print("Emote Usage:")
+    print(emote_usage)
+    
     save_user_list_to_file(message_count_per_user, 'user_message_counts.txt')
+    append_totals_to_file(message_count_per_user, 'user_message_counts.txt')
